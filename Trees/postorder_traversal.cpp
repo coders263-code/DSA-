9ec31c86fb8b0467b6c3e800b0c17a364fef3fc1
coders263-code <coders263@gmail.com>
2025-11-13 22:35:41 +0500

    postOrdertraversa

---
+++ Trees/postorder_traversal.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value){
+        data=value;
+        left=right=nullptr;
+
+    }
+};static int idx=-1;
+Node* buildtree(vector<int>preorder){
+    idx++;
+    if(preorder[idx]==-1) return nullptr;
+    Node* root=new Node(preorder[idx]);
+    root->left=buildtree(preorder);
+    root->right=buildtree(preorder);
+    return root;
+
+
+}
+void postOrder(Node* root){
+    if(root==nullptr){
+        return;
+    }
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<endl;
+}
+int main(){
+    vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=buildtree(preorder);
+    postOrder(root);
+    cout<<endl;
+
+}
