4d6df44a48779510a5d7b2b18d621baffd98066e
coders263-code <coders263@gmail.com>
2025-11-17 22:38:15 +0500

    edit in sum

--- Trees/sum_nodes_in_Tree.cpp
+++ Trees/sum_nodes_in_Tree.cpp
@@ -22,8 +22,7 @@ Node* root=new Node(preorder[idx]);
 root->left=buildtree(preorder);
 root->right=buildtree(preorder);
 return root;
-}
-//Sum of all nodes
+}// function for sum
 int sum(Node* root){
     if(root==nullptr){
         return 0;
@@ -37,6 +36,6 @@ int main(){
     vector<int>preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root=buildtree(preorder);
     // cout<<root->data;
-    cout<<"Sum of Nodes :"<<sum(root);
+    cout<<"Sum of Nodes in tree: "<<sum(root);
     
 }
